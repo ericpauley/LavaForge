6b08de8ff8f74da60d6d8643d4bc87eff5fac671
zonedabone <zonedabone@gmail.com>
2012-03-03 21:33:30 -0500

    Update for 1.1-R5

--- src/zonedabone/LavaForge/LavaForge.java
+++ src/zonedabone/LavaForge/LavaForge.java
@@ -1,13 +1,16 @@
 package zonedabone.LavaForge;
 
-import org.bukkit.event.Event;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Furnace;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.FurnaceBurnEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class LavaForge extends JavaPlugin {
+public class LavaForge extends JavaPlugin implements Listener {
 
-	private final LavaForgeInventoryListener inventoryListener = new LavaForgeInventoryListener(this);
-	
 	@Override
 	public void onDisable() {
 		// TODO Auto-generated method stub
@@ -16,10 +19,24 @@ public class LavaForge extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		
-		PluginManager pm = this.getServer().getPluginManager();
-		pm.registerEvent(Event.Type.FURNACE_BURN,  inventoryListener, Event.Priority.Normal,  this);
 
+		PluginManager pm = this.getServer().getPluginManager();
+		pm.registerEvents(this, this);
+
+	}
+
+	public void onFurnaceBurn(FurnaceBurnEvent e) {
+		if (e.getFuel().getType() == Material.LAVA_BUCKET) {
+			final Block furnace = e.getBlock();
+			this.getServer().getScheduler()
+					.scheduleSyncDelayedTask(this, new Runnable() {
+						public void run() {
+							Furnace furn = (Furnace) furnace.getState();
+							furn.getInventory().setItem(1,
+									new ItemStack(Material.BUCKET, 1));
+						}
+					});
+		}
 	}
 
 }
